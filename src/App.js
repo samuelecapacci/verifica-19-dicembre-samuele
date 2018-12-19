75fc1b65fd2a676c0b0773d494c0169131d97f19
samuele <samuele.capacci@libero.it>
2018-12-19 12:03:22 +0100

    first-commit-già-in-lavorazione

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Barra from './components/barra/Barra';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photo: [],
+    };
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/photos/1')
+      .then(response => response.json())
+      .then(json => this.setState({
+        photo: json
+      })
+      )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="App-header">
+          <Barra />
+        </div>
+        <div className="App-body">
+          <div>
+            <div className="id">ID: </div>{this.state.photo.id}
+          </div>
+          <div>
+            <div className="title">TITLE: </div>{this.state.photo.title}
+          </div>
+          <div>
+            <img className='indirizzo' src={this.state.photo.url} />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/foto/Foto.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Foto extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
