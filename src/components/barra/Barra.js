a5588fa6a8054b6bbb3a091bcb4cc636abb50b86
samuele <samuele.capacci@libero.it>
2018-12-19 13:00:42 +0100

    navbar-aggiustata

--- src/components/barra/Barra.js
+++ src/components/barra/Barra.js
@@ -10,8 +10,7 @@ export default class Barra extends React.Component {
     return (
       <div className="Barra">
         <Navbar color="warning">
-          <NavbarBrand href="/">Verifica-19-Dicembre</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
+          <NavbarBrand href="/">Verifica-19-Dicembre</NavbarBrand> {/* unico collegamento cliccabile che riporta alla home */}
         </Navbar>
       </div>
     );
