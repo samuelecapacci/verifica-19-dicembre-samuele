02442a2562dad339b6e6750c9fbe13e25b6773bf
samuele <samuele.capacci@libero.it>
2018-12-19 12:40:42 +0100

    aggiunti-commenti&aggiustamento-css

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photo: [],
+      photo: {} /* oggetto photo */
     };
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/photos/1')
+    fetch('https://jsonplaceholder.typicode.com/photos/1')  /* fetch alla foto con id '1' */
       .then(response => response.json())
       .then(json => this.setState({
         photo: json
@@ -20,23 +20,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header">  {/* header con barra */}
           <Barra />
         </div>
-        <div className="App-body">
+        <div className="App-body">  {/* body con id, titolo e foto da url */}
           <div>
-            <div className="id">ID: </div>{this.state.photo.id}
+            <div className="id">ID: </div>{this.state.photo.id} {/* id foto preso da fetch */}
           </div>
           <div>
-            <div className="title">TITLE: </div>{this.state.photo.title}
+            <div className="title">TITLE: </div>{this.state.photo.title} {/* titolo foto preso da fetch */}
           </div>
           <div>
             <img className='indirizzo' src={this.state.photo.url} />
           </div>
         </div>
-        <div className="App-footer">
-        <p className="tag">Samuele Capacci</p>
-        <p className="tag">19/12/2018</p>
+        <div className="App-footer"> {/* footer con nome e data */}
+          <p className="tag">Samuele Capacci</p>
+          <p className="tag">19/12/2018</p>
         </div>
       </div>
     );
--- src/components/barra/Barra.js
+++ src/components/barra/Barra.js
@@ -1,36 +1,15 @@
 import React from 'react';
 import {
-  Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+  NavbarBrand
 } from 'reactstrap';
 
 export default class Barra extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <div className="Barra">
-        <Navbar color="light" light expand="md">
+        <Navbar color="warning">
           <NavbarBrand href="/">Verifica-19-Dicembre</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
         </Navbar>
